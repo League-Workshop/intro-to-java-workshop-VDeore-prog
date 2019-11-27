ad054b675d4433b8393f980badd3e6af1ac5bbad
League Student <league@iMac-13.attlocal.net>
2019-11-27 15:28:33 -0800

    day 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,18 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score= 0;
 		// 2.  Ask the user a question 
-		
+		speak("Spell maneuver");
+				
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(sentence.equalsIgnoreCase("maneuver")){
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
@@ -17,4 +20,4 @@ public class QuizGame {
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
-}
+	}
